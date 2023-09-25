4ab9a261913a54a3aa92b14b73cb4efce2156910
lucasoneves <casluhc@gmail.com>
2023-09-25 19:34:59 -0300

    Adding font Inter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title='Centro Pokémon' />
       <body>
         <Main />
         <NextScript />
