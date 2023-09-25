83786183b1a89cf571343c5f48335a2926d16938
lucasoneves <casluhc@gmail.com>
2023-09-25 20:12:17 -0300

    Footer styles

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,10 @@
+import styles from './Footer.module.scss';
+
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
-        Todas as marcas e ilustrações utilizadas são de seus resepctivos donos.
+        Todas as marcas e ilustrações utilizadas são de seus respectivos donos.
       </p>
     </footer>
   );
